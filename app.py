12762ef3007b1666cc2889126c19ad8a7bf409df
Akash <akashshekara1212@gmail.com>
2023-04-13 00:38:51 -0500

    something

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import torch
-from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from segment_anything import SamPredictor, sam_model_registry
 import cv2
 import numpy as np
 import base64
@@ -7,7 +7,7 @@ def init():
     global model
     sam = sam_model_registry["default"](checkpoint="sam_vit_h_4b8939.pth")
     sam.to("cuda")
-    model = SamAutomaticMaskGenerator(sam)
+    model = SamPredictor(sam)
 
 def inference(model_inputs):
     global model
@@ -18,7 +18,5 @@ def inference(model_inputs):
     img_original = base64.b64decode(image_string)
     img_as_np = np.frombuffer(img_original, dtype=np.uint8)
     image = cv2.imdecode(img_as_np, flags=1)
-    masks = model.generate(image)
-    for mask in masks:
-        mask["segmentation"] = mask["segmentation"].tolist()
-    return {"masks":masks}
+    mode.set_image(image)
+    return {"embedding": model.get_image_embedding()}
--- test.py
+++ test.py
@@ -9,7 +9,5 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image_string = base64.b64encode(cv2.imencode('.jpg', image)[1]).decode()
 
 out = banana.run("api_key","model_key",{"image":image_string})
-masks = out["modelOutputs"][0]["masks"]
 
-for mask in masks:
-    mask["segmentation"] = np.array(mask["segmentation"])
+print(out)
